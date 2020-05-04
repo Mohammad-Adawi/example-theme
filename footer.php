d56e79a116cc51dc09349ece4a22207b3ba82f09
Mohammad <adawi83@hotmail.com>
2020-05-04 16:09:36 -0600

    footer

--- footer.php
+++ footer.php
@@ -6,8 +6,13 @@
       <?php echo date( 'Y' ); // Use PHP to keep the year current! Less work for us! ?>
       TECHCareers by Manpower
     </p>
+    <?php if ( function_exists( 'output_hello_world' ) ) : // Check if the plugin function exists. ?>
+      <aside>
+        <?php output_hello_world(); ?>
+      </aside>
+    <?php endif; ?>
   </footer>
   <?php wp_footer(); ?>
-
 </body>
 </html>
+
